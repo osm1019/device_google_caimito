2a3ce6d73be935df05773bef8c22de5e042a7d79
Alexander Koskovich <akoskovich@pm.me>
2024-12-04 19:56:52 -0500

    Add prebuilts for satellite SOS feature
    This feature will not be offered unless GMS is present.
    
    Change-Id: If0746362fb0e60ebe0c99cad554ce9a55f3fb7a1

--- device-lineage.mk
+++ device-lineage.mk
@@ -21,6 +21,10 @@ PRODUCT_PACKAGES += \
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
+# Satellite
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/conf/allowlist_satellite.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_satellite.xml
+
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
