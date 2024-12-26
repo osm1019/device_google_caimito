2ecefb1139f4fa5c63397d96d4d4f1d77fde18eb
Oscar <osm1019@gmail.com>
2024-12-25 20:25:03 -0800

    caimito: mistify

--- lineage_komodo.mk
+++ lineage_komodo.mk
@@ -16,6 +16,60 @@ $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 $(call inherit-product, device/google/zumapro/lineage_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 
+#MistOS Extras
+# UDFPS support 
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+# Blur
+TARGET_ENABLE_BLUR := true
+# BCR (Call recording)
+TARGET_PREBUILT_BCR := true
+# Lawnchair (Pixel launcher by default)
+#TARGET_INCLUDE_PIXEL_LAUNCHER := true
+#TARGET_DEFAULT_PIXEL_LAUNCHER := true
+#TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+# Exclude Aperture camera
+PRODUCT_NO_CAMERA := true
+# Google Camera App
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+# Custom package installer
+#TARGET_USE_CUSTOM_PACKAGE_INSTALLER := true
+# Live wallpapers
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP  := true
+# Now Playing
+TARGET_SUPPORTS_NOW_PLAYING := true
+# Clear Calling
+TARGET_SUPPORTS_CLEAR_CALLING := true
+# Call Recording Support
+TARGET_SUPPORTS_CALL_RECORDING := true
+# Pixel Carrier Settings
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_PIXEL_IMS := true
+TARGET_INCLUDE_PIXEL_EUICC := true
+TARGET_INCLUDE_CARRIER_SERVICES := true
+# A bunch of Pixel stuff
+USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_DREAMLINER := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
+TARGET_SUPPORTS_ADPATIVE_CHARGING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_CAMERA := true
+TARGET_GOOGLE_CAMERA_LARGE_RAM := true
+TARGET_PIXEL_EXPERIENCE_2023 := true
+# GMS
+WITH_GMS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+# Mist OS Flags
+MIST_BUILD_TYPE := OFFICIAL
+MISTOS_MAINTAINER := OscarM
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9 Pro XL
